425c5293279ba7763f0cdd8f9b949ab593771847
yuriy05 <dilayyura@gmail.com>
2024-09-04 23:19:20 +0300

    feat: add more reusability for component

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const starRatingContainer = {
   display: 'flex',
@@ -6,29 +7,50 @@ const starRatingContainer = {
   gap: '4px',
 };
 
-const starRatingFont = {
-  fontSize: '16px',
-  fontFamily: 'sans-serif',
-  lineHeight: 1,
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  size: PropTypes.number,
+  color: PropTypes.string,
+  class: PropTypes.string,
+  defaultRating: PropTypes.number,
 };
 
-function StarRating({ maxLength = 5 }) {
-  const [rating, setRating] = useState(1);
+function StarRating({
+  maxRating = 5,
+  size = 24,
+  color = '#fcc419',
+  className = '',
+  defaultRating = 0,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
-  const handleClick = (index) => setRating(index + 1);
   const handleMouseEnter = (index) => setTempRating(index + 1);
   const handleMouseLeave = () => setTempRating(null);
 
+  const handleRating = (rating) => {
+    setRating(rating);
+  };
+
+  const starRatingFont = {
+    fontSize: '16px',
+    fontFamily: 'sans-serif',
+    lineHeight: '1',
+    color,
+    margin: '0',
+  };
+
   return (
-    <div style={starRatingContainer}>
-      {Array.from({ length: maxLength }, (_, i) => (
+    <div style={starRatingContainer} className={className}>
+      {Array.from({ length: maxRating }, (_, i) => (
         <Star
           filled={i < (tempRating || rating)}
           key={i}
-          onClick={() => handleClick(i)}
+          onClick={() => handleRating(i + 1)}
           onMouseEnter={() => handleMouseEnter(i)}
           onMouseLeave={() => handleMouseLeave(i)}
+          color={color}
+          size={size}
         />
       ))}
       <p style={starRatingFont}>{rating}</p>
@@ -36,27 +58,59 @@ function StarRating({ maxLength = 5 }) {
   );
 }
 
-function Star({ filled, onClick, onMouseEnter, onMouseLeave }) {
+function Star({
+  filled,
+  onClick,
+  onMouseEnter,
+  onMouseLeave,
+  color,
+  size,
+}) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: 'block',
+    cursor: 'pointer',
+  };
+
   return (
-    <svg
-      className="star"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      stroke="#000"
-      height={24}
-      width={24}
+    <span
+      role="button"
+      style={starStyle}
       onClick={onClick}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      fill={filled ? 'yellow' : 'none'}
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="{2}"
-        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-      />
-    </svg>
+      {filled ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke={color}
+          fill={filled ? color : 'none'}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="{2}"
+            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke={color}
+          fill="none"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="{2}"
+            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+          />
+        </svg>
+      )}
+    </span>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import StarRating from './StarRating';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
-
